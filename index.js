1606d70a970f9226c3b6993aa9e43b462a109d96
Brock Ellis <brock@sharproot.com>
2019-02-28 18:20:34 -0600

    Adds 2 endpoints for locations and catalogitems

--- index.js
+++ index.js
@@ -14,36 +14,47 @@ app.use(basicAuth({
 
 function getUnauthorizedResponse(req) {
   return req.auth
-    ? ('Obviously ' + req.auth.user + ':' + req.auth.password + ' isn\'t cool enough to access this inf. Loser.')
+    ? ('Obviously ' + req.auth.user + ':' + req.auth.password + ' isn\'t cool enough to access this info. Loser.')
     : 'Dude, you didn\'t provide the correct digits.'
 }
 
-var defaultClient = SquareConnect.ApiClient.instance;
-var oauth2 = defaultClient.authentications['oauth2'];
+const defaultClient = SquareConnect.ApiClient.instance;
+const oauth2 = defaultClient.authentications['oauth2'];
 oauth2.accessToken = process.env.SOOPER_SEKRET_KEY;
 
-var square_api = new SquareConnect.LocationsApi();
-
-
-app.listen(PORT, () => {
-  console.log('We are live on ' + PORT);
-});
+var api_instance; 
 
 app.get('/', (req, res, next) => {
   res.send('Hello there. Please supply the correct URL and super secret password to continue.');
 });
 
-app.get("/get", (req, res, next) => {
+app.get("/locations", (req, res, next) => {
+  api_instance = new SquareConnect.LocationsApi();
 
-  square_api.listLocations().then(function (data) {
-  
+  api_instance.listLocations().then(function (data) {
     console.log('API called successfully. Returned data: ' + JSON.stringify(data));
     res.json(data);
   
   }, function (error) {
     console.error(error);
   });
+});
 
+app.get("/catalogitems", (req, res, next) => {
+  api_instance  = new SquareConnect.CatalogApi();
 
-  // res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+  var opts = {
+    // 'cursor': "cursor_example", // String | The pagination cursor returned in the previous response. Leave unset for an initial request. See [Paginating results](#paginatingresults) for more information.
+    'types': "ITEM" // String | An optional case-insensitive, comma-separated list of object types to retrieve, for example `ITEM,ITEM_VARIATION,CATEGORY`.  The legal values are taken from the [CatalogObjectType](#type-catalogobjecttype) enumeration, namely `\"ITEM\"`, `\"ITEM_VARIATION\"`, `\"CATEGORY\"`, `\"DISCOUNT\"`, `\"TAX\"`, `\"MODIFIER\"`, or `\"MODIFIER_LIST\"`.
+  };
+  api_instance.listCatalog(opts).then(function (data) {
+    console.log('API called successfully. Returned data: ' + data);
+    res.json(data);
+  }, function (error) {
+    console.error(error);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('We are live on ' + PORT);
 });
